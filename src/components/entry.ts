befc32a637d520e9dd26736512246faa24e9fcbf
Noah Fernandes <noahdecoco@Noahs-MacBook-Pro.local>
2018-09-29 19:52:23 +0200

    strict data type

--- src/components/entry.ts
+++ src/components/entry.ts
@@ -1,10 +1,10 @@
 const title: string = "flint";
 const subtitle: string = "A firestarter for webapps";
 
-const h1 = document.createElement("h1");
+const h1: HTMLElement = document.createElement("h1");
 h1.innerHTML = title;
 
-const p = document.createElement("p");
+const p: HTMLElement = document.createElement("p");
 p.innerHTML = subtitle;
 
 document.body.appendChild(h1);
