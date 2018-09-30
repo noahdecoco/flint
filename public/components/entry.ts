a8465bc8ea1302c5c4602c9643c5122eaf6edce7
Noah Fernandes <noahdecoco@Noahs-MacBook-Pro.local>
2018-09-30 20:39:42 +0200

    added lint integation pre commit hook

--- public/components/entry.ts
+++ public/components/entry.ts
@@ -1,19 +1,19 @@
-const h1Content: string = '🔥 flint'
-const p1Content: string = 'A firestarter for webapps'
+const h1Content: string = "🔥 flint";
+const p1Content: string = "A firestarter for webapps";
 const p2Content: string =
-  '<a href="http://localhost:3000/">Server listening on port 3000</a>'
+  "<a href='http://localhost:3000/'>Server listening on port 3000</a>";
 
 let test = "fjkdslfjs";
 
-const h1: HTMLElement = document.createElement('h1')
-h1.innerHTML = h1Content
+const h1: HTMLElement = document.createElement("h1");
+h1.innerHTML = h1Content;
 
-const p1: HTMLElement = document.createElement('p')
-p1.innerHTML = p1Content
+const p1: HTMLElement = document.createElement("p");
+p1.innerHTML = p1Content;
 
-const p2: HTMLElement = document.createElement('p')
-p2.innerHTML = p2Content
+const p2: HTMLElement = document.createElement("p");
+p2.innerHTML = p2Content;
 
-document.body.appendChild(h1)
-document.body.appendChild(p1)
-document.body.appendChild(p2)
+document.body.appendChild(h1);
+document.body.appendChild(p1);
+document.body.appendChild(p2);
