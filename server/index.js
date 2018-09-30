d6ad1614356d35966f864a1749de26790efad2c0
Noah Fernandes <noahdecoco@Noahs-MacBook-Pro.local>
2018-09-30 20:42:04 +0200

    added lint integation pre commit hook

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require("express");
 
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 
 app.listen(port, () => {
-  console.log(`Express server running on port ${port}`)
-})
+  console.log(`Express server running on port ${port}`);
+});
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`<p>🔥 Flint server is standing by.</p>
-  <p><a href="http://localhost:8080/">Frontend on port 8080</a></p>`)
-})
+  <p><a href="http://localhost:8080/">Frontend on port 8080</a></p>`);
+});
